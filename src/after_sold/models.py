344132d239efdc484e73d9c47f2f3aefac2781ee
loheagn <linan_123@buaa.edu.cn>
2019-05-24 14:58:23 +0800

    add comment model

--- src/after_sold/models.py
+++ src/after_sold/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from login_manage.models import *
+from watch_buy.models import *
+
 
 # Create your models here.
+
+class Comment(models.Model):
+    content = models.TextField(null=False, editable=False)  # 用户发表评论后不可修改！
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=False)
+    good = models.ForeignKey(Goods, on_delete=models.CASCADE, null=False)
+    quality = models.FloatField(null=False)
+    c_time = models.DateTimeField(auto_now_add=True, editable=False)  # 评论创建时间
+
+    def __str__(self):
+        return str(self.content)
+
+    class Meta:
+        verbose_name = "这是一条评论"
+        verbose_name_plural = verbose_name
